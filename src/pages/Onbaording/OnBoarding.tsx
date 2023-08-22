6c11462338616904b1b4ed4bd8ff187fe2aa8778
최정운 <choijung4631@naver.com>
2023-08-23 01:09:15 +0900

    refactor : container-view structure 구조로 변경
    1. container : state 변경 등 비지니스 로직이 포함된 component / view가 container에 포함됨
    2. view : props를 입력받아 화면에 표시되는 component

---
+++ src/pages/Onbaording/OnBoarding.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import OnBoardingContainer from '../../containers/OnboardingContainer';
+
+function OnBoarding(): JSX.Element {
+  return <OnBoardingContainer />;
+}
+
+export default OnBoarding;
