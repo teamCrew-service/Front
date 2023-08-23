621d363f67e513349e440d532b8484658f4fc52b
최정운 <choijung4631@naver.com>
2023-08-23 16:36:44 +0900

    feat : naver map 연결 및 FindCrew page 생성

---
+++ src/pages/FindCrew/FindCrew.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import './FindCrew.css';
+
+function FindCrew(): JSX.Element {
+  useEffect(() => {
+    function initMap(): void {
+      const mapOption = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        maxZoom: 14,
+        scaleControl: false,
+      };
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const map = new naver.maps.Map('map', mapOption);
+    }
+    initMap();
+  });
+  return <div id="map" className="container map-container" />;
+}
+
+export default FindCrew;
