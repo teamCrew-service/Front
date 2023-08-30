ef64cc29275f7faf0ea669b83e633a9b2e38befa
최정운 <choijung4631@naver.com>
2023-08-30 20:13:18 +0900

    feat : react-naver-maps 도입 및 적용

--- src/pages/findcrew/FindCrew.tsx
+++ src/pages/findcrew/FindCrew.tsx
@@ -1,74 +1,31 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import './FindCrewStyle.css';
-import spots from './mockData';
+import React, { useState, useEffect } from 'react';
+import { Container, NaverMap, useNavermaps } from 'react-naver-maps';
 
 function FindCrew(): JSX.Element {
-  const mapDiv = useRef<HTMLDivElement | null>(null);
-  const [currentPosition, setCurrentPosition] = useState<{ latitude: number; longitude: number }>({
-    latitude: 0,
-    longitude: 0,
-  });
-  const initMap = useCallback((latitude: number, longitude: number): void => {
-    if (mapDiv.current !== null) {
-      const mapOption = {
-        center: new naver.maps.LatLng(latitude, longitude),
-        maxZoom: 14,
-        zoom: 7,
-        minZoom: 7,
-        scaleControl: false,
-      };
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const map = new naver.maps.Map(mapDiv.current, mapOption);
-      const markers = [];
-      for (let i = 0; i < spots.length; i += 1) {
-        const markerOption = {
-          position: new naver.maps.LatLng(spots[i].lat, spots[i].lng),
-          map,
-          title: spots[i].title,
-        };
-        const marker = new naver.maps.Marker(markerOption);
-        const infowindow = new naver.maps.InfoWindow({
-          content: spots[i].title,
-        });
-
-        // 마커에 마우스 호버 시에 정보를 표시하는 이벤트 리스너 추가
-        naver.maps.Event.addListener(marker, 'mouseover', () => {
-          infowindow.open(map, marker);
-        });
-
-        // 마커에 마우스가 벗어날 때 인포윈도우 닫기
-        naver.maps.Event.addListener(marker, 'mouseout', () => {
-          infowindow.close();
-        });
-
-        markers.push(marker);
-      }
-    }
+  const [loading, setLoading] = useState<boolean>(false);
+  const [myLatLng, setMyLatLng] = useState<{ lat: number; lng: number }>({ lat: 0, lng: 0 });
+  const navermaps = useNavermaps();
+  useEffect(() => {
+    const setMyPosition = (): void => {
+      navigator.geolocation.getCurrentPosition(position => {
+        setMyLatLng({ lat: position.coords.latitude, lng: position.coords.longitude });
+        setLoading(true);
+      });
+    };
+    setMyPosition();
   }, []);
 
-  useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          setCurrentPosition({ latitude: position.coords.latitude, longitude: position.coords.longitude });
-        },
-        error => {
-          console.log('Error getting geolocation:', error.message);
-        },
-      );
-    } else {
-      console.log('geolocation을 지원하지 않는 브라우저입니다.');
-    }
-  }, []);
-
-  useEffect(() => {
-    initMap(currentPosition.latitude, currentPosition.longitude);
-  }, [currentPosition]);
-
-  return currentPosition.latitude === 0 && currentPosition.longitude === 0 ? (
-    <div style={{ margin: 'auto 0px' }}>Loading...</div>
-  ) : (
-    <div ref={mapDiv} className="container map-container" />
+  return (
+    <Container style={{ width: '100%', height: '812px', border: 'none' }}>
+      {loading && (
+        <NaverMap
+          defaultCenter={new navermaps.LatLng(myLatLng.lat, myLatLng.lng)}
+          defaultZoom={12}
+          maxZoom={14}
+          minZoom={10}
+        />
+      )}
+    </Container>
   );
 }
 
