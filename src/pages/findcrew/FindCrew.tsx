21f92b79a8ae66fdcdb6991fda53ea1a462c5fc6
최정운 <choijung4631@naver.com>
2023-09-03 12:51:33 +0900

    feat : clustering 적용
    - react-naver-maps 삭제
    - 순수 javascript class 이용 clustering 구현

--- src/pages/findcrew/FindCrew.tsx
+++ src/pages/findcrew/FindCrew.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Container, NaverMap, useNavermaps } from 'react-naver-maps';
+import useMarkerClustering from 'util/useMarkerClustering';
 
 function FindCrew(): JSX.Element {
+  // 위치 정보 로딩 여부
   const [loading, setLoading] = useState<boolean>(false);
+  // // 현재 위치 정보
   const [myLatLng, setMyLatLng] = useState<{ lat: number; lng: number }>({ lat: 0, lng: 0 });
-  const navermaps = useNavermaps();
+  // 정리
+  // 1. 페이지 로딩 완료 시마다 api를 통해 data 가져오기
+  // 2. 가져온 data를 기반으로 newCluster 생성
+
   useEffect(() => {
     const setMyPosition = (): void => {
       navigator.geolocation.getCurrentPosition(position => {
@@ -15,17 +20,23 @@ function FindCrew(): JSX.Element {
     setMyPosition();
   }, []);
 
-  return (
-    <Container style={{ width: '100%', height: '812px', border: 'none' }}>
-      {loading && (
-        <NaverMap
-          defaultCenter={new navermaps.LatLng(myLatLng.lat, myLatLng.lng)}
-          defaultZoom={12}
-          maxZoom={14}
-          minZoom={10}
-        />
-      )}
-    </Container>
+  useEffect(() => {
+    if (myLatLng.lat !== 0 && myLatLng.lng !== 0) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const map = new window.naver.maps.Map('map', {
+        center: new window.naver.maps.LatLng(myLatLng.lat, myLatLng.lng),
+        zoom: 14,
+        maxZoom: 14,
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const newCluster = useMarkerClustering(map);
+    }
+  }, [myLatLng]);
+
+  return loading ? (
+    <div id="map" style={{ width: '100%', height: '812px', border: 'none' }} />
+  ) : (
+    <div style={{ width: '100%', height: '812px', border: 'none' }}>loading</div>
   );
 }
 
---
+++ src/util/useMarkerClustering.ts
@@ -0,0 +1,82 @@
+import { makeMarkerClustering } from 'pages/findcrew/MarkerClustering';
+import spots from 'pages/findcrew/mockData';
+
+function useMarkerClustering(map?: naver.maps.Map): any {
+  const MarkerClustering = makeMarkerClustering(window.naver);
+
+  const htmlMarker1 = {
+    content:
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-1.png);background-size:contain;"></div>',
+    size: new window.naver.maps.Size(40, 40),
+    anchor: new window.naver.maps.Point(20, 20),
+  };
+  const htmlMarker2 = {
+    content:
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-2.png);background-size:contain;"></div>',
+    size: new window.naver.maps.Size(40, 40),
+    anchor: new window.naver.maps.Point(20, 20),
+  };
+  const htmlMarker3 = {
+    content:
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-3.png);background-size:contain;"></div>',
+    size: new window.naver.maps.Size(40, 40),
+    anchor: new window.naver.maps.Point(20, 20),
+  };
+  const htmlMarker4 = {
+    content:
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-4.png);background-size:contain;"></div>',
+    size: new window.naver.maps.Size(40, 40),
+    anchor: new window.naver.maps.Point(20, 20),
+  };
+  const htmlMarker5 = {
+    content:
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-5.png);background-size:contain;"></div>',
+    size: new window.naver.maps.Size(40, 40),
+    anchor: new window.naver.maps.Point(20, 20),
+  };
+
+  const data = spots;
+
+  const markers: naver.maps.Marker[] = [];
+  const infoWindows: naver.maps.InfoWindow[] = [];
+
+  function getClickHandler(marker: naver.maps.Marker, infoWindow: naver.maps.InfoWindow) {
+    return () => {
+      if (map !== undefined) {
+        infoWindow.open(map, marker);
+      }
+    };
+  }
+
+  for (let i = 0, ii = data.length; i < ii; i += 1) {
+    const spot = data[i];
+    const latlng = new window.naver.maps.LatLng(spot.lat, spot.lng);
+    const marker = new window.naver.maps.Marker({
+      position: latlng,
+    });
+    const infoWindow = new window.naver.maps.InfoWindow({
+      content: spot.title,
+    });
+    markers.push(marker);
+    infoWindows.push(infoWindow);
+    naver.maps.Event.addListener(marker, 'click', getClickHandler(marker, infoWindow));
+  }
+
+  const newCluster = new MarkerClustering({
+    minClusterSize: 2, // 클러스터가 표시될 최소 마커 수
+    maxZoom: 14, // maxZoom보다 작을 경우 클러스터 표시
+    map, // 클러스터가 표시될 맵
+    markers, // 클러스터링 될 마커들
+    disableClickZoom: false, // 더블클릭하여 확대할건지에 대한 여부
+    gridSize: 120, // px단위, gridSize로 정의된 그리드 크기 내에서 마커들을 그룹화하여 하나의 클러스터로 표시
+    icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
+    indexGenerator: [10, 100, 200, 500, 1000],
+    stylingFunction: (clusterMarker: any, count: any) => {
+      // eslint-disable-next-line no-param-reassign
+      clusterMarker.getElement().querySelector('div:first-child').innerText = count;
+    },
+  });
+  return newCluster;
+}
+
+export default useMarkerClustering;
