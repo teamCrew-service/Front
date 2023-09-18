11ca19b194ec2585a581d223ec74f2ccbfe85874
zadragon <mase0804@naver.com>
2023-09-18 23:39:32 +0900

    api 만들기

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,6 +14,24 @@ interface Information {
   location: string | null;
 }
 
+interface MakeCrew {
+  userId: number;
+  crewType: string;
+  category: string;
+  crewAddress: string;
+  crewMemberInfo: string;
+  crewTimeInfo: string;
+  crewAgeInfo: string;
+  crewSignup: boolean;
+  crewTitle: string;
+  crewContent: string;
+  crewMaxMember: number;
+  latitude: number;
+  longtitude: number;
+  question1: string;
+  question2: string;
+}
+
 const login = {
   firstLogin: async <T = LoginMessage>(information: Information): Promise<T> => {
     const { data } = await instance.put<T>('api/auth/info', information);
@@ -25,6 +43,12 @@ const login = {
   },
 };
 
+const meet = {
+  makeCrew: async (payload: MakeCrew) => {
+    const { data } = await instance.post('/api/crew/create', { payload });
+    return data;
+  },
+};
 const naverMap = {};
 
-export { login, naverMap };
+export { login, naverMap, meet };
